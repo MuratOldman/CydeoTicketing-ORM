4318e07786f146c84c82f195024c2b4c3ee50abb
MuratOldman <muratkara15102@gmail.com>
2022-11-13 13:35:11 -0500

    I added @Column(nullable = false,updatable = false) to BaseEntity class based on your need.

--- src/main/java/com/example/entity/BaseEntity.java
+++ src/main/java/com/example/entity/BaseEntity.java
@@ -16,15 +16,22 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @Column(nullable = false,updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false,updatable = false)
     private Long insertUserId;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
 
     private Boolean isDeleted=false;
 
     @PrePersist
     public void onPrePersist(){
+
         this.insertDateTime=LocalDateTime.now();
         this.lastUpdateDateTime=LocalDateTime.now();
         this.insertUserId=1L;
